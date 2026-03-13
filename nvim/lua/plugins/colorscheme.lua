d61d44902543d70bfbc6f2fbc9efb5b4d4d61499
ymtdir <ymtdir0407@gmail.com>
2026-03-13 21:11:38 +0900

    style: onedarkの背景を透明に変更

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -8,5 +8,12 @@ return {
   {
     "olimorris/onedarkpro.nvim",
     priority = 1000,
+    config = function()
+      require("onedarkpro").setup({
+        options = {
+          transparency = true,
+        },
+      })
+    end,
   },
 }
